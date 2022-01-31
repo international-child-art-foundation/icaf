d015902f0f4f399f53744735cdbf68130b836290
vinays84 <vinay.singh84@gmail.com>
2022-01-30 19:07:49 -0500

    Added Volume button and functionality

--- js/icaf-main.js
+++ js/icaf-main.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
-    window.wcf = $('video')[0];
-    wcf.pause();
-    wcf.currentTime = 185;
+    // window.wcf = $('video')[0];
+    // wcf.pause();
+    // wcf.currentTime = 185;
     /*
         currentTime
         playbackRate
      */
+    let videoElm = $('video')[0];
+    videoElm.volume = 0.2;
+    let $volumeDiv = $('#volumeControl');
+    let $inputs = $('#volumeControl').find('input').first().hide().prevObject;
+    $volumeDiv.click(_ => {
+        $inputs.toggle();
+        videoElm.muted = !videoElm.muted 
+    });
+
 
 });

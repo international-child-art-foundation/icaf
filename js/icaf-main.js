e9dd3af8c07e31381ffd0a54bfc420058a8420da
vinays84 <vinay.singh84@gmail.com>
2022-02-12 22:32:24 -0500

    Added Carousel

--- js/icaf-main.js
+++ js/icaf-main.js
@@ -12,8 +12,23 @@ $(document).ready(function() {
     let $inputs = $('#volumeControl').find('input').first().hide().prevObject;
     $volumeDiv.click(_ => {
         $inputs.toggle();
-        videoElm.muted = !videoElm.muted 
+        videoElm.muted = !videoElm.muted
     });
 
 
+
+    let $carousel = $('#youngArtists').carousel({
+        interval: 3000
+    });
+    $('#slidePrev > circle').click(_ => $carousel.carousel('prev'));
+    $('#slideNext > circle').click(_ => $carousel.carousel('next'));
+
+    // For debugging
+    window.c = $carousel;
+
+    setTimeout(_ => {
+        $('body').append(`<div style="height:300px;text-align:center"><br><br> I am an unfinished &lt;div&gt;.</div>`);
+        $(document).scrollTop($(document).height());
+        setTimeout(_ => $carousel.carousel('pause'), 10);
+    }, 50);
 });

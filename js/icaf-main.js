f5b20d6a2b564fd19d739301a933115fc4bfc245
Vinay <vinay.singh84@gmail.com>
2022-05-30 14:31:20 -0400

    Added WCF section

--- js/icaf-main.js
+++ js/icaf-main.js
@@ -1,29 +1,13 @@
 $(document).ready(function() {
-    /*
     let videoElm = $('video')[0];
-    videoElm.volume = 0.2;
+    videoElm.volume = 0.4;
     let $volumeDiv = $('#volumeControl');
-    let $inputs = $('#volumeControl').find('input').first().hide().prevObject;
-    $volumeDiv.click(_ => {
+    $('#videoContainer')
+        .mouseenter(() => $volumeDiv.fadeIn(200))
+        .mouseleave(() => $volumeDiv.fadeOut(200));
+    let $inputs = $volumeDiv.find('img');
+    $volumeDiv.click(() => {
         $inputs.toggle();
         videoElm.muted = !videoElm.muted
     });
-
-
-
-    let $carousel = $('#youngArtists').carousel({
-        interval: 3000
-    });
-    $('#slidePrev > circle').click(_ => $carousel.carousel('prev'));
-    $('#slideNext > circle').click(_ => $carousel.carousel('next'));
-
-    // For debugging
-    window.c = $carousel;
-
-    setTimeout(_ => {
-        $('body').append(`<div style="height:300px;text-align:center"><br><br> I am an unfinished &lt;div&gt;.</div>`);
-        $(document).scrollTop($(document).height());
-        setTimeout(_ => $carousel.carousel('pause'), 10);
-    }, 50);
-    */
 });

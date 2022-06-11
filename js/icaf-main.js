57b43d37752cdcbe0c652648e01f5994fc49d7e7
Vinay <vinay.singh84@gmail.com>
2022-06-11 13:30:54 -0400

    Welcome to ICAF

--- js/icaf-main.js
+++ js/icaf-main.js
@@ -10,4 +10,25 @@ $(document).ready(function() {
         $inputs.toggle();
         videoElm.muted = !videoElm.muted
     });
+
+
+});
+
+//DEBUG
+const DEBUG_MODE = false;
+$(document).ready(function() {
+    if (DEBUG_MODE) $('#cfMetadata').show();
+    let startTime = Date.now();
+    const duration = 10;
+
+    let updateDisplay = function() {
+        $('#time').text((((Date.now() - startTime) / 1000) % duration).toFixed(1));
+        //console.log( Number($('#f3').css('opacity')).toFixed(2) )
+        $('#t1').text(Number($('#debug1').css('opacity')).toFixed(2));
+        $('#t2').text(Number($('#debug2').css('opacity')).toFixed(2));
+        $('#t3').text(Number($('#debug3').css('opacity')).toFixed(2));
+        $('#t4').text(Number($('#debug4').css('opacity')).toFixed(2));
+    }
+
+    setInterval(updateDisplay, 100);
 });

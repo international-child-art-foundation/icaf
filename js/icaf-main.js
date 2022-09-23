94894918367c03c48da01d84d573949cd87efb2b
Vinay <vinay.singh84@gmail.com>
2022-09-22 20:05:52 -0400

    More nav updates

--- js/icaf-main.js
+++ js/icaf-main.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     let videoElm = $('video')[0];
     videoElm.volume = 0.4;
     let $volumeDiv = $('#volumeControl');
-    $('#videoContainer')
-        .mouseenter(() => $volumeDiv.fadeIn(200))
-        .mouseleave(() => $volumeDiv.fadeOut(200));
+    // $('#videoContainer')
+    //     .mouseenter(() => $volumeDiv.fadeIn(200))
+    //     .mouseleave(() => $volumeDiv.fadeOut(200));
     let $inputs = $volumeDiv.find('img');
     $volumeDiv.click(() => {
         $inputs.toggle();

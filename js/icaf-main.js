77e95a3e77c64edeb4fbd26e21f546ea015b6a12
Vinay <vinay.singh84@gmail.com>
2022-11-13 15:19:08 -0500

    Membership updates

--- js/icaf-main.js
+++ js/icaf-main.js
@@ -6,9 +6,6 @@ $(document).ready(function () {
     videoElm.volume = 0.4;
     videoElm.currentTime = videoStart;
     let $volumeDiv = $('#volumeControl');
-    // $('#videoContainer')
-    //     .mouseenter(() => $volumeDiv.fadeIn(200))
-    //     .mouseleave(() => $volumeDiv.fadeOut(200));
     let $inputs = $volumeDiv.find('img');
     $volumeDiv.click(() => {
         $inputs.toggle();
@@ -20,23 +17,4 @@ $(document).ready(function () {
 
     let carousel = new ICAFCarousel('.carousel-wrapper');
     carousel.play();
-});
-
-//DEBUG
-const DEBUG_MODE = false;
-$(document).ready(function () {
-    if (DEBUG_MODE) $('#cfMetadata').show();
-    let startTime = Date.now();
-    const duration = 10;
-
-    let updateDisplay = function () {
-        $('#time').text((((Date.now() - startTime) / 1000) % duration).toFixed(1));
-        //console.log( Number($('#f3').css('opacity')).toFixed(2) )
-        $('#t1').text(Number($('#debug1').css('opacity')).toFixed(2));
-        $('#t2').text(Number($('#debug2').css('opacity')).toFixed(2));
-        $('#t3').text(Number($('#debug3').css('opacity')).toFixed(2));
-        $('#t4').text(Number($('#debug4').css('opacity')).toFixed(2));
-    };
-
-    setInterval(updateDisplay, 100);
 });

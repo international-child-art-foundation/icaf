319be8f96851a54aac6529c109e0aa0d1dbe3424
Vinay <vinay.singh84@gmail.com>
2022-11-17 22:39:58 -0500

    Added a basic functional timeline element so we can start building the UI

---
+++ js/ICAFTimeline.js
@@ -0,0 +1,30 @@
+class ICAFTimeline {
+    constructor($wrapperElement, options) {
+        if ($wrapperElement.constructor.name === 'String') $wrapperElement = $($wrapperElement);
+        options = Object.assign({
+
+        }, options);
+        this.$timeline = $wrapperElement.children();
+        this.$rows = this.$timeline.children();
+        this.activeIdx = 0;
+        this.$rows.eq(0).addClass('active');
+
+        $wrapperElement.on('wheel', e => {
+            e.preventDefault();
+            let delta = Math.sign(e.originalEvent.deltaY)
+            console.log(delta, this.activeIdx + delta);
+            if (this.$rows[this.activeIdx + delta]) {
+                this.updateActiveIdx(this.activeIdx + delta);
+            }
+
+        });
+    }
+
+    updateActiveIdx(idx) {
+        let rowGap = +(this.$timeline.css('rowGap').replace('px', ''));
+        let offset = this.$rows.get().slice(0, idx).reduce((sum, row) => sum - row.offsetHeight - rowGap, 0);
+        this.$timeline.css('marginTop', offset);
+        this.$rows.removeClass('active').eq(idx).addClass('active')
+        this.activeIdx = idx;
+    }
+}

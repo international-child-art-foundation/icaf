f12a696de0bfa3bfa5d7be9527b57580d8f7fe6b
Vinay <vinay.singh84@gmail.com>
2022-11-23 19:36:02 -0500

    Small updates

--- js/ICAFTimeline.js
+++ js/ICAFTimeline.js
@@ -12,7 +12,7 @@ class ICAFTimeline {
         $wrapperElement.on('wheel', e => {
             e.preventDefault();
             let delta = Math.sign(e.originalEvent.deltaY)
-            console.log(delta, this.activeIdx + delta);
+            // console.log(delta, this.activeIdx + delta);
             if (this.$rows[this.activeIdx + delta]) {
                 this.updateActiveIdx(this.activeIdx + delta);
             }
@@ -21,10 +21,11 @@ class ICAFTimeline {
     }
 
     updateActiveIdx(idx) {
-        let rowGap = +(this.$timeline.css('rowGap').replace('px', ''));
-        let offset = this.$rows.get().slice(0, idx).reduce((sum, row) => sum - row.offsetHeight - rowGap, 0);
-        this.$timeline.css('marginTop', offset);
+        // let rowGap = +(this.$timeline.css('rowGap').replace('px', ''));
+        // let offset = this.$rows.get().slice(0, idx).reduce((sum, row) => sum - row.offsetHeight - rowGap, 0);
         this.$rows.removeClass('active').eq(idx).addClass('active')
+        let offset = -this.$rows.get(idx).offsetTop;
+        this.$timeline.css('marginTop', offset);
         this.activeIdx = idx;
     }
 }

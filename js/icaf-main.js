918a270f8d8dbcbc64c5e3942ba1c69f5f2ac5a5
Vinay <vinay.singh84@gmail.com>
2023-04-17 10:49:57 -0400

    Main page update and cleanup

--- js/icaf-main.js
+++ js/icaf-main.js
@@ -1,19 +1,21 @@
 import ICAFCarousel from './ICAFCarousel.js';
 
 $(document).ready(function () {
-    const videoStart = 10.5;
-    let videoElm = $('video')[0];
-    videoElm.volume = 0.4;
-    videoElm.currentTime = videoStart;
-    let $volumeDiv = $('#volumeControl');
-    $volumeDiv.click(() => {
-        $volumeDiv.find('img').toggleClass('hidden');
-        videoElm.muted = !videoElm.muted
-    });
-    let $playControl = $('#playControl');
-    $playControl.click(() => {
-        $playControl.find('> div').toggleClass('hidden');
-        videoElm.paused ? videoElm.play() : videoElm.pause();
+    $('.video-container').each((_, container) => {
+        let $container = $(container);
+        let videoElm = $container.find('video')[0];
+        videoElm.volume = 0.4;
+        videoElm.currentTime = +videoElm.getAttribute('data-start');
+        let $volumeDiv = $container.find('.volume-control');
+        $volumeDiv.click(() => {
+            $volumeDiv.find('img').toggleClass('hidden');
+            videoElm.muted = !videoElm.muted
+        });
+        let $playControl = $container.find('.play-control');
+        $playControl.click(() => {
+            $playControl.find('> div').toggleClass('hidden');
+            videoElm.paused ? videoElm.play() : videoElm.pause();
+        });
     });
 
     let carousel = new ICAFCarousel('.carousel-wrapper');

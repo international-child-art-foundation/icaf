dd94efd31ff610b8a32819703659ae1dfffcd479
Vinay <vinay.singh84@gmail.com>
2023-04-17 10:49:27 -0400

    timeline initial

--- js/ICAFTimeline.js
+++ js/ICAFTimeline.js
@@ -1,31 +1,66 @@
 class ICAFTimeline {
+
+    static buildEntry($timeline, entryData) {
+        let { year, imgSrc, imgLink, description } = entryData;
+        let imgAlt = (imgSrc.match(/\/(\w+)?\.\w+$/) || [, ''])[1];
+        let imgHtml = `<img src="${imgSrc}" alt="${imgAlt}" >`;
+        if (imgLink) imgHtml = `<a target="_blank" href="${imgLink}">${imgHtml}</a>`;
+
+        $timeline.append(`
+            <div class="entry">
+                <div class="year">${year}</div>
+                <div class="line"></div>
+                <div class="image">${imgHtml}</div>
+                <div class="text">${description}</div>
+            </div>
+        `);
+    }
+
     constructor($wrapperElement, options) {
         if ($wrapperElement.constructor.name === 'String') $wrapperElement = $($wrapperElement);
-        options = Object.assign({
+        this.options = Object.assign({}, ICAFTimeline.DEFAULT_OPTIONS, options);
+        this.$timeline = $wrapperElement.find(('#' + this.options.timelineId).replace(/^\#+/, '#'));
 
-        }, options);
-        this.$timeline = $wrapperElement.children();
-        this.$rows = this.$timeline.children();
-        this.activeIdx = 0;
-        this.$rows.eq(0).addClass('active');
+        (async () => {
+            if (this.options.dataJson && this.options.buildEntry) {
+                this.jsonData = await $.get(this.options.dataJson);
+                this.jsonData.forEach(data => this.options.buildEntry(this.$timeline, data));
+                this.options.onInit(this);
+            }
+            this.$rows = this.$timeline.children(this.options.timelineEntrySelector);
+            this.updateActiveIdx(this.options.startIdx);
+        })();
 
         $wrapperElement.on('wheel', e => {
             e.preventDefault();
             let delta = Math.sign(e.originalEvent.deltaY)
             // console.log(delta, this.activeIdx + delta);
-            if (this.$rows[this.activeIdx + delta]) {
-                this.updateActiveIdx(this.activeIdx + delta);
-            }
-
+            this.moveActiveIdx(delta);
         });
     }
 
+    static DEFAULT_OPTIONS = {
+        timelineId: 'timeline',
+        timelineEntrySelector: '.entry',
+        entryHeight: 120,
+        rowGap: 47,
+        startIdx: 0,
+        offset: 0,
+        onInit: () => { },
+        onUpdate: () => { }
+    };
+
+    moveActiveIdx(delta) {
+        if (!this.$rows[this.activeIdx + delta]) return;
+        this.updateActiveIdx(this.activeIdx + delta);
+    }
+
     updateActiveIdx(idx) {
-        // let rowGap = +(this.$timeline.css('rowGap').replace('px', ''));
-        // let offset = this.$rows.get().slice(0, idx).reduce((sum, row) => sum - row.offsetHeight - rowGap, 0);
         this.$rows.removeClass('active').eq(idx).addClass('active')
-        let offset = -this.$rows.get(idx).offsetTop;
-        this.$timeline.css('marginTop', offset);
+        // let offset = this.options.offset - this.$rows.get(idx).offsetTop;
+        let offset = this.options.offset - idx * (this.options.entryHeight + this.options.rowGap);
+        this.$timeline.css('transform', `translateY(${offset}px)`)
         this.activeIdx = idx;
+        this.options.onUpdate(this);
     }
 }

b5c8f66cc956c4a09009642f680672f1027fdc2f
Andrew Dame <andrew_dame@outlook.com>
2023-06-03 17:05:21 -0400

    feat: utilize video.js video player

--- js/icaf-main.js
+++ js/icaf-main.js
@@ -1,23 +1,6 @@
 import ICAFCarousel from './ICAFCarousel.js';
 
 $(document).ready(function () {
-    $('.video-container').each((_, container) => {
-        let $container = $(container);
-        let videoElm = $container.find('video')[0];
-        videoElm.volume = 0.4;
-        videoElm.currentTime = +videoElm.getAttribute('data-start');
-        let $volumeDiv = $container.find('.volume-control');
-        $volumeDiv.click(() => {
-            $volumeDiv.find('img').toggleClass('hidden');
-            videoElm.muted = !videoElm.muted
-        });
-        let $playControl = $container.find('.play-control');
-        $playControl.click(() => {
-            $playControl.find('> div').toggleClass('hidden');
-            videoElm.paused ? videoElm.play() : videoElm.pause();
-        });
-    });
-
     let carousel = new ICAFCarousel('.carousel-wrapper');
     carousel.play();
 });

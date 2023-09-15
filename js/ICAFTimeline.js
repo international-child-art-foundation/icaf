8a48262070245b1eaf47f2c1efa565432eea014e
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2023-09-15 11:38:17 -0400

    Implement responsive design (#33)
    * feat: Implement responsive design

--- js/ICAFTimeline.js
+++ js/ICAFTimeline.js
@@ -33,10 +33,54 @@ class ICAFTimeline {
         })();
 
         $wrapperElement.on('wheel', e => {
+            let delta = Math.sign(e.originalEvent.deltaY);
+        
+            if ((this.isAtStart() && delta < 0) || (this.isAtEnd() && delta > 0)) {
+                // Allow the default behavior (scrolling the outer page)
+                return;
+            }
+        
+            // Otherwise, handle the timeline scrolling
             e.preventDefault();
-            let delta = Math.sign(e.originalEvent.deltaY)
-            // console.log(delta, this.activeIdx + delta);
             this.moveActiveIdx(delta);
+        });
+        
+        // Adding touch event handlers for mobile support
+        let startY;
+        let cumulativeDelta = 0;
+        const SWIPE_THRESHOLD = 50;
+
+        let startTime;
+        $wrapperElement.on('touchstart', e => {
+            startY = e.originalEvent.touches[0].pageY;
+            startTime = new Date().getTime();
+            cumulativeDelta = 0; // Reset the cumulative delta on a new touch start
+        });
+        
+        $wrapperElement.on('touchmove', e => {
+            const moveEndY = e.originalEvent.changedTouches[0].pageY;
+            const delta = startY - moveEndY;
+            cumulativeDelta += delta; // Accumulate the delta
+            if ((this.isAtStart() && delta < 0) || (this.isAtEnd() && delta > 0)) {
+                // Allow the default behavior (scrolling the outer page)
+                return;
+            }        
+            e.preventDefault();
+
+
+            // Only act on the delta when it exceeds the threshold and reset the cumulative delta
+            if (Math.abs(cumulativeDelta) > SWIPE_THRESHOLD) {
+                // Determine swipe direction (up or down) and move active index accordingly
+                if (cumulativeDelta > 0) { // Swiped up
+                    this.moveActiveIdx(1);
+                } else { // Swiped down
+                    this.moveActiveIdx(-1);
+                }
+                
+                cumulativeDelta = 0; // Reset after handling
+            }
+            
+            startY = moveEndY; // Update startY for the next touchmove
         });
     }
 
@@ -58,10 +102,19 @@ class ICAFTimeline {
 
     updateActiveIdx(idx) {
         this.$rows.removeClass('active').eq(idx).addClass('active')
-        // let offset = this.options.offset - this.$rows.get(idx).offsetTop;
         let offset = this.options.offset - idx * (this.options.entryHeight + this.options.rowGap);
         this.$timeline.css('transform', `translateY(${offset}px)`)
         this.activeIdx = idx;
+        
         this.options.onUpdate(this);
     }
+
+    isAtStart() {
+        return this.activeIdx === 0;
+    }
+    
+    isAtEnd() {
+        return this.activeIdx === this.$rows.length - 1;
+    }
+    
 }
